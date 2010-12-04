a5695b7e210dc2d8393191179432c39700d5fc48
Jesse Proudman <jesse.proudman@blueboxgrp.com>
2010-12-03 17:52:43 -0800

    Update the deploy.rb to use a more recent version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,60 +1,78 @@
+# Capistrano Multistage Support
+# Uncomment the following lines if you'd like Multistage Capistrano support.
+# set :default_stage, "staging"
+# set :stages, %w(production staging)
+# require 'capistrano/ext/multistage'
+
+# Include Bundler Extensions
+# Comment out the following line if you're not using Bundler.
+require "bundler/capistrano"
+
 # RVM Settings
+# Use either the latest RVM settings, or the legacy settings depending on your local RVM version.
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
-set :rvm_ruby_string, "1.9.2"
+set :rvm_ruby_string, "ree-1.8.7-2010.02"
 set :rvm_type, :system
-
-# Server Name
-set :server_name, "___.blueboxgrid.com"
+# set :default_environment, {
+#  'PATH' => "/usr/local/rvm/gems/ree-1.8.7-2010.02/bin:/usr/local/rvm/bin:/bin:$PATH",
+#  'RUBY_VERSION' => 'ree',
+#  'GEM_HOME'     => '/usr/local/rvm/gems/ree-1.8.7-2010.02',
+#  'GEM_PATH'     => '/usr/local/rvm/gems/ree-1.8.7-2010.02',
+#}
 
 # Application Settings
-set :application, ""
-set :repository,  "git://github.com/blueboxjesse/demo_rails3_deploy_app.git"
+set :application, "APP NAME"
+set :deploy_to, "/home/deploy/rails_apps/#{application}"
 
-# Generall Settings
+# Repo Settings
+set :repository,  "git@github.com:/"
 set :scm, "git"
-default_run_options[:pty] = true
 set :checkout, 'export'
 
-set :user, "deploy"
+# General Settings
+default_run_options[:pty] = true
+set :keep_releases, 5
 
-set :deploy_to, "/srv/#{application}"
-set :use_sudo, false
-
-role :app, server_name
-role :web, server_name
-role :db,  server_name, :primary => true
-
-set :keep_releases, 3
+# Hooks
 after "deploy", "deploy:cleanup"
+after "deploy:update_code", "deploy:web:update_maintenance_page"
 after "deploy:update_code", "deploy:secondary_symlink"
-after 'deploy:secondary_symlink', 'bundler:bundle_new_release'
 
 namespace :deploy do
+  task :secondary_symlink, :except => { :no_release => true } do
+    run "rm -f #{release_path}/config/database.yml"
+    run "ln -s #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
 
-	task :secondary_symlink do
-		run "rm -f #{release_path}/config/database.yml"
-		run "ln -s #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-	end
+  task :restart, :except => { :no_release => true } do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
 
-	task :restart do
-		run "touch #{deploy_to}/current/tmp/restart.txt"
-	end
-
-	task :start do
-		run "touch #{deploy_to}/current/tmp/restart.txt"
-	end
+  task :start, :except => { :no_release => true } do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
 end
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'vendor_bundle')
-    release_dir = File.join(current_release, 'vendor/bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
- 
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} && bundle install --deployment"
+# Disable the built in disable command and setup some intelligence so we can have images.
+disable_path = "#{shared_path}/system/maintenance/"
+namespace :deploy do
+  namespace :web do
+    desc "Disables the website by putting the maintenance files live."
+    task :disable, :except => { :no_release => true } do
+      on_rollback { run "mv #{disable_path}index.html #{disable_path}index.disabled.html" }
+      run "mv #{disable_path}index.disabled.html #{disable_path}index.html"
+    end
+
+    desc "Enables the website by disabling the maintenance files."
+    task :enable, :except => { :no_release => true } do
+        run "mv #{disable_path}index.html #{disable_path}index.disabled.html"
+    end
+
+    desc "Copies your maintenance from public/maintenance to shared/system/maintenance."
+    task :update_maintenance_page, :except => { :no_release => true } do
+      run "rm -rf #{shared_path}/system/maintenance/; true"
+      run "cp -r #{release_path}/public/maintenance #{shared_path}/system/"
+    end
   end
 end
